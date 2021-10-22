dc6af3207e3acd8507e6924ccabff0bc57c38d22
AlexPetrov <saberbringers@gmail.com>
2021-10-22 21:06:48 +0300

    home work

--- app/src/main/java/com/alexpetrov/loftmoney/MainActivity.java
+++ app/src/main/java/com/alexpetrov/loftmoney/MainActivity.java
@@ -1,14 +1,44 @@
 package com.alexpetrov.loftmoney;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ItemsAdapter itemsAdapter = new ItemsAdapter();
+
+    private ItemsAdapter adapter;
+
+    private List<Item> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setAdapter(itemsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setItems(items);
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
