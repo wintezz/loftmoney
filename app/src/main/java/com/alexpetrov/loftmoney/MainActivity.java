2cf6694c39e5b526f272ddac18785fa5aff9b52f
AlexPetrov <saberbringers@gmail.com>
2021-10-24 15:17:56 +0300

    fix, DividerItemDecoration

--- app/src/main/java/com/alexpetrov/loftmoney/MainActivity.java
+++ app/src/main/java/com/alexpetrov/loftmoney/MainActivity.java
@@ -1,8 +1,10 @@
 package com.alexpetrov.loftmoney;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Item> items;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(itemsAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter.setItems(items);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(getResources().getDrawable(R.layout.marcup));
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
 
     }
 
